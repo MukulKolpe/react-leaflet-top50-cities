fa96a6487023c3500875b9334c884ec8d4576f9f
Mukul Kolpe <mukulkolpe45@gmail.com>
2022-10-27 22:11:00 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { MapContainer, CircleMarker, TileLayer, Tooltip } from "react-leaflet";
+import Leaflet from "leaflet";
+import "leaflet/dist/leaflet.css";
 
-function App() {
+const url =
+  "https://public.opendatasoft.com/api/records/1.0/search/?dataset=geonames-all-cities-with-a-population-1000&q=&lang=EN&rows=50&sort=population&facet=timezone&facet=country";
+
+const corner1 = Leaflet.latLng(-90, -200);
+const corner2 = Leaflet.latLng(90, 200);
+const bounds = Leaflet.latLngBounds(corner1, corner2);
+export default function App() {
+  const [cities, setCities] = useState();
+
+  useEffect(() => {
+    async function fetchCities() {
+      try {
+        const res = await fetch(url);
+        setCities(await res.json());
+      } catch (err) {
+        console.error(err);
+      }
+    }
+
+    fetchCities();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3 style={{ textAlign: "center" }}>
+        50 Most Populated Cities in the World
+      </h3>
+      <MapContainer
+        center={[0, 0]}
+        zoom={2}
+        minZoom={1.5}
+        style={{ height: "100vh", width: "100%" }}
+        maxBounds={bounds}
+        maxBoundsViscosity={0.4}
+      >
+        <TileLayer
+          url="http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          noWrap={true}
+        />
+        {cities &&
+          cities.records.map((city, i) => {
+            return (
+              <CircleMarker
+                center={[
+                  city.fields["coordinates"][0],
+                  city.fields["coordinates"][1],
+                ]}
+                radius={1.5 * (city.fields.population / 1000000)}
+                fillOpacity={0.4}
+                stroke={true}
+                key={city.recordid}
+              >
+                <Tooltip direction="top" offset={[0, -2]} opacity={1}>
+                  <span>
+                    {i + 1}. {city.fields.name} : {city.fields.population}
+                  </span>
+                </Tooltip>
+              </CircleMarker>
+            );
+          })}
+      </MapContainer>
     </div>
   );
 }
-
-export default App;
